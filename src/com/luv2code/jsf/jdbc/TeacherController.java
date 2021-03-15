12396b41e44d8d41a4a78fb9cf9f3b1933e2a3a7
kkatsara <kkatsara@IIL2533.athens.intrasoft-intl.private>
2021-03-15 13:40:14 +0200

    start working on teacher object

--- src/com/luv2code/jsf/jdbc/TeacherController.java
+++ src/com/luv2code/jsf/jdbc/TeacherController.java
@@ -1,14 +1,151 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.luv2code.jsf.jdbc;
 
-/**
- *
- * @author kkatsara
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+//import java.util.logging.Logger;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
 public class TeacherController {
-    
+
+	private List<Teacher> teachers;
+	private TeacherDbUtil teacherDbUtil;
+     //   private Logger logger = Logger.getLogger(getClass().getName());
+	
+	public TeacherController() throws Exception {
+		teachers = new ArrayList<>();
+                
+		teacherDbUtil = TeacherDbUtil.getInstance();
+	}
+	
+	public List<Teacher> getTeachers() {
+		return teachers;
+	}
+        
+	public void loadTeachers() {
+
+		//logger.info("Loading teachers");
+		
+		teachers.clear();
+
+		try {
+			
+			// get all teachers from database
+			teachers = teacherDbUtil.getTeachers();
+			
+		} catch (Exception exc) {
+			// send this to server logs
+		//	logger.log(Level.SEVERE, "Error loading teachers", exc);
+			
+			// add error message for JSF page
+			addErrorMessage(exc);
+		}
+	}
+        
+	public String addTeacher(Teacher theTeacher) {
+
+	//	logger.info("Adding teacher: " + theTeacher);
+
+		try {
+			
+			// add teacher to the database
+			teacherDbUtil.addTeacher(theTeacher);
+			
+		} catch (Exception exc) {
+			// send this to server logs
+			//logger.log(Level.SEVERE, "Error adding teachers", exc);
+			                 System.out.println(exc);
+			// add error message for JSF page
+			addErrorMessage(exc);
+
+			return null;
+		}
+		
+		return "list-teachers?faces-redirect=true";
+	}
+
+	public String loadTeachers(int teacherId) {
+		
+	//	logger.info("loading teacher: " + teacherId);
+		
+		try {
+			// get teacher from database
+			Teacher theTeacher = teacherDbUtil.getTeacher(teacherId);
+			
+			// put in the request attribute ... so we can use it on the form page
+			ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();		
+
+			Map<String, Object> requestMap = externalContext.getRequestMap();
+			requestMap.put("teacher", theTeacher);	
+			
+		} catch (Exception exc) {
+			// send this to server logs
+			//logger.log(Level.SEVERE, "Error loading teacher id:" + teacherId, exc);
+			  System.out.println(exc);
+			// add error message for JSF page
+			addErrorMessage(exc);
+			
+			return null;
+		}
+				
+		return "update-teacher-form.xhtml";
+	}	
+	
+	public String updateStudent(Teacher theTeacher) {
+
+		//logger.info("updating teacher: " + theTeacher);
+		
+		try {
+			
+			// update student in the database
+			teacherDbUtil.updateTeacher(theTeacher);
+			
+		} catch (Exception exc) {
+			// send this to server logs
+			//logger.log(Level.SEVERE, "Error updating techer: " + theTeacher, exc);
+			  System.out.println(exc);
+			// add error message for JSF page
+			addErrorMessage(exc);
+			
+			return null;
+		}
+		
+		return "list-teacher?faces-redirect=true";		
+	}
+	
+	public String deleteTeacher(int teacherId) {
+
+		//logger.info("Deleting student id: " + teacherId);
+		
+		try {
+
+			// delete the student from the database
+			teacherDbUtil.deleteTeacher(teacherId);
+			
+		} catch (Exception exc) {
+			// send this to server logs
+			//logger.log(Level.SEVERE, "Error deleting teacher id: " + teacherId, exc);
+			  System.out.println(exc);
+			// add error message for JSF page
+			addErrorMessage(exc);
+			
+			return null;
+		}
+		
+		return "list-teadcher";	
+	}	
+	
+	private void addErrorMessage(Exception exc) {
+		FacesMessage message = new FacesMessage("Error: " + exc.getMessage());
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+	
 }
--- src/com/luv2code/jsf/jdbc/TeacherDbUtil.java
+++ src/com/luv2code/jsf/jdbc/TeacherDbUtil.java
@@ -5,10 +5,229 @@
  */
 package com.luv2code.jsf.jdbc;
 
-/**
- *
- * @author kkatsara
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class TeacherDbUtil {
-    
+
+	private static TeacherDbUtil instance;
+	private DataSource dataSource;
+	private String jndiName = "java:comp/env/jdbc/student_tracker";
+	
+	public static TeacherDbUtil getInstance() throws Exception {
+		if (instance == null) {
+			instance = new TeacherDbUtil();
+		}
+		
+		return instance;
+	}
+	
+	private TeacherDbUtil() throws Exception {		
+		dataSource = getDataSource();
+	}
+
+	private DataSource getDataSource() throws NamingException {
+		Context context = new InitialContext();
+		
+		DataSource theDataSource = (DataSource) context.lookup(jndiName);
+		
+		return theDataSource;
+	}
+		
+	public List<Teacher> getTeachers() throws Exception {
+
+		List<Teacher> teachers = new ArrayList<>();
+
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			myConn = getConnection();
+
+			String sql = "select * from teacher order by LName";
+
+			myStmt = myConn.createStatement();
+
+			myRs = myStmt.executeQuery(sql);
+
+			// process result set
+			while (myRs.next()) {
+				
+				// retrieve data from result set row
+				int id = myRs.getInt("id");
+				String firstName = myRs.getString("Name");
+				String lastName = myRs.getString("LName");
+				String email = myRs.getString("mail");
+
+				// create new teacher object
+				Teacher tempTeacher = new Teacher(id, firstName, lastName,
+						email);
+
+				// add it to the list of teachers
+				teachers.add(tempTeacher);
+			}
+			
+			return teachers;		
+		}
+		finally {
+			close (myConn, myStmt, myRs);
+		}
+	}
+
+	public void addTeacher(Teacher theTeacher) throws Exception {
+
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+
+		try {
+			myConn = getConnection();
+
+			String sql = "insert into teacher (Name, LName, mail) values (?, ?, ?)";
+
+			myStmt = myConn.prepareStatement(sql);
+
+			// set params
+			myStmt.setString(1, theTeacher.getFirstName());
+			myStmt.setString(2, theTeacher.getLastName());
+			myStmt.setString(3, theTeacher.getEmail());
+			
+			myStmt.execute();			
+		}
+		finally {
+			close (myConn, myStmt);
+		}
+		
+	}
+	
+	public Teacher getTeacher(int teacherId) throws Exception {
+	
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			myConn = getConnection();
+
+			String sql = "select * from teacher where id=?";
+
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set params
+			myStmt.setInt(1, teacherId);
+			
+			myRs = myStmt.executeQuery();
+
+			Teacher theTeacher = null;
+			
+			// retrieve data from result set row
+			if (myRs.next()) {
+				int id = myRs.getInt("id");
+				String firstName = myRs.getString("Name");
+				String lastName = myRs.getString("LName");
+				String email = myRs.getString("mail");
+
+				theTeacher = new Teacher(id, firstName, lastName,
+						email);
+			}
+			else {
+				throw new Exception("Could not find teacher id: " + teacherId);
+			}
+
+			return theTeacher;
+		}
+		finally {
+			close (myConn, myStmt, myRs);
+		}
+	}
+	
+	public void updateTeacher(Teacher theTeacher) throws Exception {
+
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+
+		try {
+			myConn = getConnection();
+
+			String sql = "update teacher "
+						+ " set Name=?, LName=?, mail=?"
+						+ " where id=?";
+
+			myStmt = myConn.prepareStatement(sql);
+
+			// set params
+			myStmt.setString(1, theTeacher.getFirstName());
+			myStmt.setString(2, theTeacher.getLastName());
+			myStmt.setString(3, theTeacher.getEmail());
+			myStmt.setInt(4, theTeacher.getId());
+			
+			myStmt.execute();
+		}
+		finally {
+			close (myConn, myStmt);
+		}
+		
+	}
+	
+	public void deleteTeacher(int teacherId) throws Exception {
+
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+
+		try {
+			myConn = getConnection();
+
+			String sql = "delete from teacher where id=?";
+
+			myStmt = myConn.prepareStatement(sql);
+
+			// set params
+			myStmt.setInt(1, teacherId);
+			
+			myStmt.execute();
+		}
+		finally {
+			close (myConn, myStmt);
+		}		
+	}	
+	
+	private Connection getConnection() throws Exception {
+
+		Connection theConn = dataSource.getConnection();
+		
+		return theConn;
+	}
+	
+	private void close(Connection theConn, Statement theStmt) {
+		close(theConn, theStmt, null);
+	}
+	
+	private void close(Connection theConn, Statement theStmt, ResultSet theRs) {
+
+		try {
+			if (theRs != null) {
+				theRs.close();
+			}
+
+			if (theStmt != null) {
+				theStmt.close();
+			}
+
+			if (theConn != null) {
+				theConn.close();
+			}
+			
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}	
 }
